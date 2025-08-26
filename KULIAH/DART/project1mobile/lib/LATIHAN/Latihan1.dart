646a1e4566a1f7c5afb221037b49004ce6f27cb2
JiandzahLukman <lukxx0108@gmail.com>
2025-08-26 11:39:53 +0900

    TugasLatihan1

---
+++ KULIAH/DART/project1mobile/lib/LATIHAN/Latihan1.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 20;
+  String name = "Jiandzah";
+  double height = 164.5;
+  num weight = 50;
+  dynamic NPM = "07352211037";
+
+  print(
+    "nama saya $name\n umur saya $age tahun\n tinggi saya $height cm\n berat saya $weight kg\n NPM saya $NPM",
+  );
+}
